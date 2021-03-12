5078a43d019af9fe7d9ebcb359db560312c36fca
Timothy Claassens <tim.claassens@gmail.com>
2021-03-13 03:30:17 +1300

    Removed cors

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
-import { NestApplicationOptions } from '@nestjs/common';
-
-const corsOptions: CorsOptions = {
-  origin: 'http://localhost:4200',
-};
-
-const applicationOptions: NestApplicationOptions = {
-  cors: corsOptions,
-};
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, applicationOptions);
+  const app = await NestFactory.create(AppModule);
   await app.listen(3000);
 }
 bootstrap();
